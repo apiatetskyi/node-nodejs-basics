cc077efd6b7f83cc885141dbaf5ede81bc4273ae
a.piatetskyi <a.piatetskyi@gmail.com>
2022-06-05 23:48:47 +0300

    feat: implement read module

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,24 @@
+import { readFile } from "fs/promises";
+
+import { getDirname } from "../utils/path.js";
+import { fileExists } from "../utils/file-exists.js";
+
+const __dirname = getDirname(import.meta.url);
+const filePath = `${__dirname}/files/fileToRead.txt`;
+
 export const read = async () => {
-    // Write your code here 
+  try {
+    const exists = await fileExists(filePath);
+
+    if (exists) {
+      const content = await readFile(filePath);
+      console.log(content.toString());
+    } else {
+      throw new Error("FS operation failed");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+read();
