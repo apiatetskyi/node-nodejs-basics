a41b714e2ab0f2367471f3fa10819a448749e3e1
a.piatetskyi <a.piatetskyi@gmail.com>
2022-06-05 21:01:31 +0300

    feat: implement copy module

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,27 @@
+import { readdir, copyFile, mkdir } from "fs/promises";
+import { getDirname } from "../utils/path.js";
+
+const __dirname = getDirname(import.meta.url);
+
 export const copy = async () => {
-    // Write your code here 
+  try {
+    await mkdir(`${__dirname}/files_copy`);
+    const entries = await readdir(`${__dirname}/files`, {
+      withFileTypes: true,
+    });
+
+    for (let entry of entries) {
+      if (entry.isFile()) {
+        copyFile(
+          `${__dirname}/files/${entry.name}`,
+          `${__dirname}/files_copy/${entry.name}`
+        );
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("FS operation failed");
+  }
 };
+
+copy();
